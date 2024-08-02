77daa13d9d7c7176b8cce0548a5810be9c67701f
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2024-08-02 21:36:02 +0300

    Link routes and views with database

--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,14 @@
-var express = require('express');
-var router = express.Router();
-
-const messages = [
-  {
-    text: 'Hi there!',
-    user: 'Amando',
-    added: new Date(),
-  },
-  {
-    text: 'Hello World!',
-    user: 'Charles',
-    added: new Date(),
-  },
-];
+const express = require('express');
+const router = express.Router();
+const messagesController = require('../controllers/messagesController');
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Mini Message Board', msgs: messages });
-});
+router.get('/', messagesController.getMessages);
 
 router.get('/new', function (req, res, next) {
   res.render('form', { title: 'Send new message' });
 });
 
-router.post('/new', function (req, res, next) {
-  const messageUser = req.body.messageUser;
-  const messageText = req.body.messageText;
-  messages.push({ text: messageText, user: messageUser, added: new Date() });
-  res.redirect('/');
-});
+router.post('/new', messagesController.createMessagePost);
 
 module.exports = router;
